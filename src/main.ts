8ceb6ea32eb06c18fcfa2ae4f1c6dbac486c5a6f
Vladislaw1 <vladzajcenko077@gmail.com>
2026-07-12 16:06:08 +0300

    added CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true,transformOptions: { enableImplicitConversion: true },}))
   await app.listen(process.env.PORT ?? 3000);
 }
